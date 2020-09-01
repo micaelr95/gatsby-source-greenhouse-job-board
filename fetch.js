bcc9f27a8f1da1354408eb48fc2f3e298b535ca6
Micael Rodrigues <micaelr95@outlook.pt>
2020-09-01 14:12:32 +0100

    build

---
+++ fetch.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const axios = require('axios');
+
+const baseUrl = `https://boards-api.greenhouse.io/v1/boards`;
+
+const fetchJobs = boardTokens => getJobs(boardTokens.map(boardToken => axios.get(`${baseUrl}/${boardToken}/jobs/`)));
+
+const fetchOffices = boardTokens => getOffices(boardTokens.map(boardToken => axios.get(`${baseUrl}/${boardToken}/offices/`)));
+
+const fetchDepartments = boardTokens => getDepartments(boardTokens.map(boardToken => axios.get(`${baseUrl}/${boardToken}/departments/`)));
+
+const getJobs = urls => Promise.all(urls).then(response => response.map(data => ({
+  url: data.config.url,
+  jobs: data.data.jobs
+}))).then(data => Promise.all(data.reduce((acc, {
+  url,
+  jobs
+}) => [...acc, ...jobs.map(async (job) => await getJob(`${url}${job.id}?questions=true`))], []))).catch(console.error);
+
+const getJob = url => axios.get(url).then(response => response.data).catch(console.error);
+
+const getOffices = async urls => await Promise.all(urls).then(response => response.map(data => data.data.offices)).then(offices => offices.reduce((acc, curr) => [...acc, ...curr], [])).catch(console.error);
+
+const getDepartments = async urls => await Promise.all(urls).then(response => response.map(data => data.data.departments)).then(departments => departments.reduce((acc, curr) => [...acc, ...curr], [])).catch(console.error);
+
+module.exports = {
+  fetchJobs,
+  fetchOffices,
+  fetchDepartments
+};
---
+++ gatsby-node.js
@@ -0,0 +1,80 @@
+"use strict";
+
+const {
+  fetchJobs,
+  fetchOffices,
+  fetchDepartments
+} = require('./fetch');
+
+const {
+  filterResponseForIds,
+  buildNodesFromResponse,
+  linkParentChildReferences,
+  linkForeignReferences
+} = require(`./normalize`);
+
+const chalk = require('chalk');
+
+const log = message => console.log('\n', message);
+
+exports.sourceNodes = async (gatsby, pluginOptions) => {
+  const {
+    actions
+  } = gatsby;
+  const {
+    createNode
+  } = actions;
+  const {
+    boardToken,
+    boardTokens = []
+  } = pluginOptions;
+  let offices, departments, jobs;
+  let nodes;
+  let boards = [];
+
+  if (!!boardToken) {
+    log(`${chalk.blue('info')} boardToken has been deprecated. Use 'boardTokens: ['boardToken]' instead.`);
+    boards = [boardToken];
+  } else {
+    boards = [...boardTokens];
+  }
+
+  try {
+    // Fetch data from the Greenhouse APIs
+    offices = await fetchOffices(boards);
+    departments = await fetchDepartments(boards);
+    jobs = await fetchJobs(boards);
+  } catch (error) {
+    log(`
+      ${chalk.red.bold('error')} Fetching data from Greenhouse failed
+    `);
+    process.exit(1);
+  }
+
+  log(`
+    ${chalk.blue('info')} fetched from Greenhouse API:
+    ${chalk.bold(jobs.length)} jobs,
+    ${chalk.bold(offices.length)} offices,
+    ${chalk.bold(departments.length)} departments
+  `); // Greenhouse API will return "No Office" and "No Department" entities with an ID of 0. Let's remove these.
+
+  offices = filterResponseForIds(offices);
+  departments = filterResponseForIds(departments);
+  jobs = filterResponseForIds(jobs); // Construct the node objects from the API responses
+
+  nodes = buildNodesFromResponse({
+    offices,
+    departments,
+    jobs
+  }); // Link Offices to their parents/children
+  // Link Departments to their parents/children
+
+  nodes = linkParentChildReferences(nodes); // Link Offices to Departments and Jobs
+  // Link Departments to Jobs
+  // Link Jobs to Departments and Offices
+
+  nodes = linkForeignReferences(nodes); // Create the nodes with Gatsby
+
+  await Promise.all(nodes.map(async node => createNode(node)));
+  return;
+};
